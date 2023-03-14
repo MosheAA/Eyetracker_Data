35fe2fedc0ecf1e56bb2035dfd47b771c640f391
Moshé Alonso <42875118+MosheAA@users.noreply.github.com>
2023-03-13 20:30:52 -0500

    Update Analisisdata.m

--- Functions/Analisisdata.m
+++ Functions/Analisisdata.m
@@ -1,7 +1,12 @@
 
-file_ID = "..\MATLAB\Eyetracker_Data-main\Data\1_all_gaze.csv"; 
-vidObj = VideoReader('..\MATLAB\Eyetracker_Data-main\Data\video_export_01-25-23-15.35.44.avi');
+file_ID = "..\MATLAB\VideoMoral\result\User 0_all_gaze.csv"; 
+vidObj = VideoReader('..\MATLAB\VideoMoral\result\video_export_02-20-23-15.47.39.avi');
 
+%% TO DO: 1. List stimulus function TSV
+%         2. Detect start of series of stimulus
+%         3. Segment data according to ongoing experiment phase 
+%         4. Select ROI
+%         4. Generate ROI-stimulus summary results
 Fs_g = 61;                                      % Sampling frequency Gaze 
 Fs_v = vidObj.FrameRate;                        % Sampling frequency Video 
 
@@ -9,7 +14,7 @@ allgaze = import_data_gaze(file_ID);                  % Import gaze data
 time_v = 0:1/Fs_g:length(allgaze)/Fs_g - (1/Fs_g);    % Time vector
 time_2 = 0:1/Fs_v:vidObj.NumFrames/Fs_v - (1/Fs_v);   % Time vector
 figure
-plot(allgaze(:,1),allgaze(:,2))
+plot(allgaze(:,2),allgaze(:,3))
 plot(allgaze(:,3),allgaze(:,4))
 %% Visualize data 
 test               = downsample(allgaze,6);
@@ -21,11 +26,17 @@ while i < 100
     ax = gca();
     imshow(vidFrame)
     hold(ax, 'on');
-    plot(ax, test(1:i,3)*1920,test(1:i,4)*1080);
+    plot(ax, test(1:i,2)*1920,test(1:i,3)*1080);
     hold(ax, 'off');
     pause(1/vidObj.FrameRate);
     i = i+1;
 end
+
+
+
+
+
+
 %%
 figure;plot(time_v, allgaze(:,1))
 hold on; plot(time_v, allgaze(:,2))
