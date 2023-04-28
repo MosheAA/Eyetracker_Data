f28e7f1400190aeeb017c08fea3e50d5cc985100
Moshé Alonso <42875118+MosheAA@users.noreply.github.com>
2023-04-27 20:50:59 -0500

    Update Analisisdata.m

--- Functions/Analisisdata.m
+++ Functions/Analisisdata.m
@@ -1,8 +1,16 @@
 %% Load gaze data
 file_ID = "..\MATLAB\Eyetracker_Data-main\Data\User 0_all_gaze.csv"; 
 allgaze = import_data_gaze(file_ID);                             % Import gaze data
-Fs_g = 61;                                                                        % Sampling frequency Gaze 
+Fs_g = 60;                                                                        % Sampling frequency Gaze 
 time_v = 0:1/Fs_g:length(allgaze)/Fs_g - (1/Fs_g);        % Time vector
+valid = ~isnan(allgaze(:,1));
+test_interp= interpn(allgaze(valid,1),allgaze(valid,2),time_v,'linear'); 
+test_interp1= interpn(allgaze(valid,1),allgaze(valid,3),time_v,'linear'); 
+locs_clicks = find(allgaze(:,6)==3); 
+
+plot(test_interp,test_interp1)
+hold on
+plot(allgaze(valid,2),allgaze(valid,3))
 %% Load TSV
 file_tsv = "..\MATLAB\Eyetracker_Data-main\Data\prueba etiquetas f - Copy_February 20, 2023_13.50.tsv.xlsx"; 
 stim = load_tsv(file_tsv);
@@ -13,45 +21,119 @@ time_2 = 0:1/Fs_v:vidObj.NumFrames/Fs_v - (1/Fs_v);   % Time vector
 
 %%         2. Detect trial start time 
 % Fixation cross
-vidObj.CurrentTime = 47;
+vidObj.CurrentTime = 47;    % Moment in which cross was detected
 vidFrame = readFrame(vidObj);
 sample = rgb2gray(vidFrame);
 [start_trial_cross] = detect_fix_cross(sample, vidObj);
-%%         3. Segment data according to ongoing experiment phase 
-
-
-%         4. Select ROI
-%         4. Generate ROI-stimulus summary results
-%% Visualize data 
-test               = downsample(allgaze,6);
-vidObj.CurrentTime = 250;
-i                  = find(time_2>=vidObj.CurrentTime,1,"first");
-
-while i < 7000
-    vidFrame = readFrame(vidObj);
-    ax = gca();
-    imshow(vidFrame)
-    hold(ax, 'on');
-    %plot(ax, test(1:i,2)*1920,test(1:i,3)*1080);
-    if start_trial_cross(i) && cert(i)>2e4
-        text(889,440,"cross")
-        text(1000,440,num2str(cert(i)))
-    end 
-    hold(ax, 'off');
-    pause(1/vidObj.FrameRate);
-
-    i = i+1;
-end
-[pks,locs] = findpeaks(start_trial_cross,"MinPeakDistance",40);
-
+% Check if correct fixation crosses were detected
 for i = 1:length(start_trial_cross)
     vidObj.CurrentTime = start_trial_cross(i)/vidObj.FrameRate;
     vidFrame = readFrame(vidObj);
     nexttile 
     imshow(vidFrame)
 end
+%%         3. Segment data according to ongoing experiment phase 
+% Cross -> 1500ms -> Stim 1 -> 12500ms -> Stim 2 -> 14000ms -> Test ->
+% click -> click -> end
+Stim_1 = start_trial_cross + (Fs_v*1.5);              % 1.5 s 
+Stim_2 = start_trial_cross + (Fs_v*(1.5+12.5));       % 1.5 + 12.5 s 
+Stim_test = start_trial_cross + (Fs_v*(1.5+12.5+14)); % 1.5 + 12.5 + 14 s 
 
+figure;plot(allgaze(:,1),allgaze(:,end))
+hold on
+scatter(start_trial_cross/Fs_v,1,50,"green","filled","^")
+scatter(Stim_1/Fs_v,1,50,"black","filled","^")
+scatter(Stim_2/Fs_v,1,50,"black","filled","^")
+scatter(Stim_test/Fs_v,1,50,"red","filled","^")
 
+%%         4. Select ROI
+figure;
+frames_to_select = [Stim_1(1)+1, Stim_2(1)+1, Stim_test(1)+10];
+roi = cell(1,6);
+k = 1;
+for i = 1:length(frames_to_select)
+    vidObj.CurrentTime = time_2(frames_to_select(i));
+    vidFrame = readFrame(vidObj);
+    ax = gca();
+    imshow(vidFrame)
+if i == 1
+    rect = drawrectangle(gca);
+    roi{k} = rect.Position;
+    k = k+1;
+elseif i == 2
+    rect = drawrectangle(gca);
+    roi{k} = rect.Position;
+    k = k+1;
+     rect = drawrectangle(gca);
+    roi{k} = rect.Position;
+    k = k+1;
+    rect = drawrectangle(gca);
+    roi{k} = rect.Position;    
+    k = k+1;
+elseif i == 3
+     rect = drawrectangle(gca);
+    roi{k} = rect.Position;
+    k = k+1;
+     rect = drawrectangle(gca);
+    roi{k} = rect.Position;
+end 
+end 
+close all
+%% 4. Generate ROI-based summary results
+in = []; 
+xq = test_interp*1920;
+yq = test_interp1*1080;
+for r = 1:length(roi)
+    xv = [roi{1,r}(1,1) roi{1,r}(1,1)+roi{1,r}(1,3)];
+    yv = [roi{1,r}(1,2) roi{1,r}(1,2)+roi{1,r}(1,4)];
+    in(:,r) = inpolygon(xq,yq,xv,yv);
+end 
+stim_1_gaze =[]; stim_2_gaze =[]; stim_Test_gaze = [];
+for i = 1:length(Stim_1)
+    % Stimulus set 1
+    ini = (Stim_1(i)./Fs_v)*Fs_g;
+    fin = (Stim_2(i)./Fs_v)*Fs_g;
+    idx = find(in(ini:fin,1));
+    stim_1_gaze(i) = numel(idx)*(1/Fs_g);
+    % Stimulus set 2
+    ini = (Stim_2(i)./Fs_v)*Fs_g;
+    fin = (Stim_test(i)./Fs_v)*Fs_g;
+    idx1 = find(in(ini:fin,2));
+    idx2 = find(in(ini:fin,3));
+    idx3 = find(in(ini:fin,4));
+
+    stim_2_gaze(i,1) = numel(idx1)*(1/Fs_g);
+    stim_2_gaze(i,2) = numel(idx2)*(1/Fs_g);
+    stim_2_gaze(i,3) = numel(idx3)*(1/Fs_g);
+    % Stimulus set 3 - Test
+    ini = (Stim_test(i)./Fs_v)*Fs_g;
+    clic_idx = find(locs_clicks>ini,1,"first");
+    fin = locs_clicks(clic_idx); %% When click!
+    idx1 = find(in(ini:fin,5));
+    idx2 = find(in(ini:fin,6));
+    stim_Test_gaze(i,1) = numel(idx1)*(1/Fs_g);
+    stim_Test_gaze(i,2) = numel(idx2)*(1/Fs_g);
+end
+%% Visualize data 
+figure;
+test               = downsample(in,6);
+test1              = downsample([xq;yq]',6);
+
+vidObj.CurrentTime = 47;
+i                  = find(time_2>=vidObj.CurrentTime,1,"first");
+
+while i < i+417
+    vidFrame = readFrame(vidObj);
+    ax = gca();
+    imshow(vidFrame)
+    hold(ax, 'on');
+    text(1000,440,num2str(i))
+    text(1000,480,num2str(test(i)))
+    plot(ax, test1(i-10:i,1),test1(i-10:i,2),"LineWidth",0.9);
+    hold(ax, 'off');
+    pause(1/vidObj.FrameRate);
+    i = i+1;
+end
 
 %% Visualize data 
 
@@ -71,4 +153,18 @@ legend({'x','y'})
 figure;plot(allgaze(:,1), allgaze(:,2))
 hold on;
 xlabel('x')
+
 ylabel('y')
+vidObj.CurrentTime = time_2(frames_to_select(i));
+vidFrame = readFrame(vidObj);
+ax = gca();
+imshow(vidFrame)
+hold on
+h = drawrectangle('Position',[roi{1,r}(1,1),roi{1,r}(1,2),roi{1,r}(1,3),roi{1,r}(1,4)],'StripeColor','r');
+
+scatter(xq(1,logical(in(:,1))'),yq(1,logical(in(:,1))'))
+
+idx = find(in(Stim_2(i):Stim_3(i),1));
+stim_2_gaze(i) = numel(idx)*(1/Fs_v);
+ 
+scatter(xq(1,logical(in(:,1))'),yq(1,logical(in(:,1))'))
